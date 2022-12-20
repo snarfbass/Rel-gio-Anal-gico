2f0131fe87a875d12cd6a342b5b1379721d803ad
mauricio oliveira <snarfalternativa@hotmail.com>
2022-12-19 22:33:08 -0300

    Mais um projeto legal finalizado .

--- script.js
+++ script.js
@@ -9,7 +9,21 @@ function updateClock(){
     let minute = now.getMinutes();
     let second = now.getSeconds();
 
+   
     digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`
+
+    //Agora vou começar a fazer o relogio analogico funcionar.
+   
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60 ) * minute) - 90;
+    let hDeg = ((360 / 12 ) * hour) - 90;
+
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`; 
+    hElement.style.transform = `rotate(${hDeg}deg)`;  
+    
+
  }
 
  function fixZero(time) {
@@ -17,3 +31,8 @@ function updateClock(){
  }
  
  setInterval(updateClock, 1000);
+ updateClock();
+
+
+
+ 
